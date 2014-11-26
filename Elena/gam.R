16bca1274b7a3a40cbb58c7ae7cb5954ff9184de
el <elena.rosskopf@gmx.de>
2014-11-26 20:36:45 +0100

    elenastuff ehm bitte local bei euch speichern, nicht, dass es morgen nicht mehr geht.

--- Elena/gam.R
+++ Elena/gam.R
@@ -24,6 +24,12 @@ smoothed4.gam <- gam(as.numeric(ts.x) ~ s(time)  +
                         COS[,3]+SIN[,3]+COS[,4]+SIN[,4]+
                         COS[,5]+SIN[,5]+COS[,6]+SIN[,6]
                       , cor=corARMA(p=2, q=2))
+
+smoothed4.gam <- gam(as.numeric(ts.x) ~ s(time, k=12)  +
+                       COS[,1]+SIN[,1]+COS[,2]+SIN[,2]+
+                       COS[,3]+SIN[,3]+COS[,4]+SIN[,4]+
+                       COS[,5]+SIN[,5]+COS[,6]+SIN[,6]
+                     , cor=corARMA(p=2, q=2))
 plot.gam(smoothed.gam, residuals=T, scheme=c(2,1),all.terms=T)
 summary(smoothed3.gam$lme)
 
@@ -31,8 +37,6 @@ anova(smoothed1.gam, smoothed2.gam, smoothed3.gam)
 AIC(smoothed3.gam$lme)
 smoothed3.gam
 plot(smoothed4.gam)
-
-
 AIC(smoothed4.gam)
 ############
 ts.plot(Nile)
--- Elena/glsfinalversion.R
+++ Elena/glsfinalversion.R
@@ -36,20 +36,27 @@ plot(ts.seasonallyadjusted, las=1, main="removed seasonal fluctuation")
 
 #check for stationarity of time series
 adf.test(ts.x, alternative = "stationary")
+adf.test (co2tranform)
+co2tranform= log(co2) # 0.69 still nonstationary
+
+yourtsdiff= diff(ts.x)
+adf.test (yourtsdiff) # now stationary ts.x!! 
+
+
 #is highly nonstationary, has a trend we need to include later in the model
 
 #4. fit the linear model, that CO2 concentrations are dependent from time ( in months ) for time period of 1958 until 2014
-data.lm = lm(co2~ year)
+datalm = lm(co2~ year)
 #in simple linear regression you cannot include a time series, we have to use the original data. 
 
 #visualize the residuals of the linear model:
 par(mfrow=c(2,2))
-plot(data.lm)
+plot(datalm)
 par(mfrow=c(1,1))
 #problems in spread of variance and normal distribution
 
 #check for normal distribution of residuals:
-shapiro.test(data.lm$residuals)
+shapiro.test(datalm$residuals)
 #highly not normally distributed 
 
 #check for differences of co2 values to 
@@ -57,11 +64,11 @@ shapiro.test(data.lm$residuals)
 
 #look at correlogram
 #of autocorrelation (seasonal trend)
-print(acf(data.lm$residuals))
-#print(acf(diff(x)))
+print(acf(datalm$residuals))
+print(acf(diff(ts.x)))
 #of partial acf (eliminates intermediate correlation between x(t) and x(t-1))
-pacf(data.lm$residuals)
-#pacf(diff(x))
+pacf(datalm$residuals)
+pacf(diff(ts.x))
 #spectrum to look for cycles 
 spectrum(data.lm$residuals)
 A=(spectrum(data.lm$residuals))
@@ -77,23 +84,27 @@ summary(data.lm)
 *****
 #6. make gls 
 library(nlme)
+
 #serial correlation model hast usually a first order autorregressive term as the lag 1 
 
-data.glsAR = gls(co2 ~ year,cor= corAR1(acf(resid(data.lm))$acf[2])) #use lag one data.acf[2]) as general 
-
+data.glsAR = gls(ts.x ~ time,cor= corAR1(acf(resid(data.lm))$acf[2])) #use lag one data.acf[2] as general 
 #try to fit other correlation structures: 
-data.glsARMA1 = gls ( co2 ~ year, cor = corARMA (c(0.2,0.2),p=1, q=1 )) #lag 1 
-data.glsARMA2 = gls (co2 ~year, cor=corARMA(c(0.2,0.2,0.2, 0.2), p=2, q=2)) #lag 2
+data.glsARMA1 = gls (ts.x ~ time, cor = corARMA (c(0.2,0.2),p=1, q=1 )) #lag 1 
+data.glsARMA2 = gls (ts.x ~ time, cor=corARMA(c(0.2,0.2,0.2, 0.2), p=2, q=2)) #lag 2
 
-anova(data.glsAR, data.gls, data.glsARMA2)
+print(anova(data.glsAR, data.glsARMA1, data.glsARMA2))
 
+data.glsARMA1
+data.glsARMA2
 #run diagnostics
 diagnostics(data.glsARMA2)
+list = as.list(diagnostics(data.glsARMA2))
+print(list)
 
-acf(data.gls$residuals))
+acf(data.gls$residuals)
 pacf(data.gls$residuals)
 summary(data.gls)
-summary(data.lm)
+summary(datalm)
 #alright, RSE of new gls is less underestimated 
 #parameter estimates are significant, trend is significant
 
@@ -108,30 +119,31 @@ plot(data.glsARMA2) #residuals looking same
 #function writing: if anova$AIC lowest, choose this as bestmodel
 #go on with seasonal effect: function: 
 #7. try to fit for seasonal component and autocorrelation with best corstruct
-dataseason.gls = gls(co2 ~ year + factor(seas), cor=corARMA( c(0.2,0.2,0.2, 0.2),p=2, q=2)) #use lag order 2 and moving average from above 
+dataseasongls = gls(ts.x ~ time + factor(seas), cor=corARMA( c(0.2,0.2,0.2, 0.2),p=2, q=2)) #use lag order 2 and moving average from above 
 
 #
 par(mfrow=c(2,1))
-acf(dataseason.gls$residuals)
-pacf(dataseason.gls$residuals)
+acf(dataseasongls$residuals)
+pacf(dataseasongls$residuals)
 
 par(mfrow=c(1,1))
-ts.plot(dataseason.gls$residuals) 
+ts.plot(dataseasongls$residuals) 
 #puuhh gar nich mal soo gut
-anova(data.glsAR, data.gls, data.glsARMA2, dataseason.gls)
+anova(data.glsARMA1, data.glsARMA2, dataseasongls)
 #oh boy cannot compare gls with different fixed effects, REML comparisons not meaningful, added a fixed effect: factor(seas)
 #so, maybe just look at AIC separately 
 
 ##AIC #smallest value best value = best model (trade off blabla)
 
 AIC(data.glsARMA2)
-AIC(dataseason.gls)
+AIC(dataseasongls)
 #write function choosing best model 
 
 
-plot(cbind(ts.x, data.glsARMA2,dataseason.gls$fitted),lty=1:2, col=c(1,2,3), main="Compare mean monthly data with gls model")
+ts.plot(cbind(ts.x, data.glsARMA2$fitted,dataseasongls$fitted),lty=1:2, col=c(1,2,3), main="Compare mean monthly data with gls model")
 legend(1960,400,c("Original", "Fitted for Autocorrelation","Fitted for seasonality"),col=c(1,2,3),lty=c(1, 2,3))
-#ohkay but looks still strange no? 
+
+
 
 
 ##try to generate better seasonal effect and add a quadratic term for the slight bending: 
@@ -147,20 +159,20 @@ for (i in 1:6) {
 #before add a quadratic term ( polynomial (x,2)), x = years will change the linear regression to include the slight curve  
 
 
-harmonized.gls<-gls(ts.x ~ time + I(time^2) +
+harmonizedgls<-gls(ts.x ~ time + I(time^2) +
                     COS[,1]+SIN[,1]+COS[,2]+SIN[,2]+
                     COS[,3]+SIN[,3]+COS[,4]+SIN[,4]+
                     COS[,5]+SIN[,5]+COS[,6]+SIN[,6],
-                  corr=corAR1(acf(dataseason.gls$residuals)$acf[2]))
-AIC(harmonized.gls) #even smaller 
+                  corr=corAR1(acf(dataseasongls$residuals)$acf[2]))
+AIC(harmonizedgls) #even smaller 
 
-harmonizedARMA.gls<-gls(ts.x ~ time + I(time^2) +
+harmonizedARMAgls<-gls(ts.x ~ time + I(time^2) +
                       COS[,1]+SIN[,1]+COS[,2]+SIN[,2]+
                       COS[,3]+SIN[,3]+COS[,4]+SIN[,4]+
                       COS[,5]+SIN[,5]+COS[,6]+SIN[,6]
                     , cor=corARMA(p=2, q=2))
-AIC(harmonizedARMA.gls) #even smaller 
-anova(harmonized.gls,harmonizedARMA.gls) #significantly better ARMA
+AIC(harmonizedARMAgls) #even smaller 
+anova(harmonizedgls,harmonizedARMAgls) #significantly better ARMA
 
 par(mfrow=c(1,1))
 ts.plot(cbind(ts.x,harmonizedARMA.gls$fitted), col=c(1,3), main="Compare mean monthly data with gls model")
@@ -208,11 +220,11 @@ now, run the auto arima for a lot easier model fitting
 
   autoarima = auto.arima(ts.x)
 #gives me perfect adjusted 
-autoarima
+print(autoarima)
 #even less AIC ! 
 fitted=fitted(autoarima)
 par(mfrow=c(1,1))
-ts.plot(cbind(ts.x, harmonizedARMA.gls$fitted), col=c(1,2))
+ts.plot(cbind(ts.x, harmonizedARMAgls$fitted), col=c(1,2))
 lines( fitted, col=3)
 legend(1960,400,c("Original",  "GLS", "Autoarima "),col=c(1,2,3), pch=c(20,20,20))
 
